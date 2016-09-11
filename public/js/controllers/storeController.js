d702388c920ad24eb554a335ca6dd52e1f25f165
Will Cook <nzwillcook@gmail.com>
2016-09-11 13:47:39 +0100

    bootstrap added via bower and bowerrc file added

--- public/js/controllers/storeController.js
+++ public/js/controllers/storeController.js
@@ -6,7 +6,7 @@ StoreController.$inject = ["TokenService", '$state', '$rootScope', 'ngCart'];
 
 function StoreController(TokenService, $state, $rootScope, ngCart) {
   
-  ngCart.setTaxRate(7.5);
+  ngCart.setTaxRate(0);
   ngCart.setShipping(2.99);
 }
 
